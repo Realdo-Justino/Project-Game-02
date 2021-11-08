faea87c08d7eaccec0d70f4949059f8f804272ba
Realdo Justino Júnior <69261890+Realdo-Justino@users.noreply.github.com>
2021-11-08 10:52:46 -0300

    First Changes

---
+++ Project-Game-Platformer-02/Assets/Player/MovePlayer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Rigidbody2D Rig;
+    bool Vfalse = false;
+    [SerializeField] float VelocidadePersonagem;
+    void Start()
+    {
+        Rig=GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 player=new Vector3(MoveH(),MoveV(),0f);
+        transform.position += player*Time.deltaTime*VelocidadePersonagem;
+    }
+    float MoveH(){
+        float movimento=(Input.GetAxis("Horizontal"));
+        return movimento;
+    }
+    float MoveV(){
+        float movimento=(Input.GetAxis("Vertical"));
+        return movimento;
+    }
+}
