5937d6d983ed665f8a57c22a5bac5ce950825246
Realdo Justino Júnior <69261890+Realdo-Justino@users.noreply.github.com>
2021-11-11 09:51:24 -0300

    Update: First Combat Experience

---
+++ Project-Game-Platformer-02/Assets/Player/Scripts/Combat/CombatPlayer.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CombatPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Transform AtkPoint;
+    [SerializeField] float AtkRange = 0.5f;
+    [SerializeField]LayerMask Enemylayers;
+    void Start()
+    {
+        AtkPoint=GameObject.Find("AtkPoint").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space)){
+            Attack();
+        }
+    }
+    void Attack(){
+        Collider2D[] HitEnemys=Physics2D.OverlapCircleAll(AtkPoint.position, AtkRange, Enemylayers);
+        foreach(Collider2D Enemy in HitEnemys){
+            Debug.Log("We hit "+Enemy);
+        }
+    }
+    void OnDrawGizmosSelected() {
+        if(AtkPoint==null) return;
+        Gizmos.DrawWireSphere(AtkPoint.position, AtkRange);
+    }
+}
