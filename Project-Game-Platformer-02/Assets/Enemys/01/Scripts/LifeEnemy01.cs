d932fbdee9499e9d20b80122357611141c0f121b
Realdo Justino Júnior <69261890+Realdo-Justino@users.noreply.github.com>
2021-11-12 16:16:50 -0300

    Update: Melle Combat

---
+++ Project-Game-Platformer-02/Assets/Enemys/01/Scripts/LifeEnemy01.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeEnemy01 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int MaxHealth=100;
+    int CurrentHealth;
+    void Start()
+    {
+        CurrentHealth = MaxHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void TakeDamage(int damage){
+        CurrentHealth -= damage;
+        Debug.Log(CurrentHealth);
+        if(CurrentHealth<=0)Die();
+    }
+    void Die(){
+        Debug.Log("Enemy Died");
+        GetComponent<SpriteRenderer>().enabled = false;
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
+    }
+}
--- Project-Game-Platformer-02/Assets/Player/Scripts/Combat/CombatPlayer.cs
+++ Project-Game-Platformer-02/Assets/Player/Scripts/Combat/CombatPlayer.cs
@@ -6,8 +6,12 @@ public class CombatPlayer : MonoBehaviour
 {
     // Start is called before the first frame update
     Transform AtkPoint;
+    float NextAttackTime=0f;
+    [SerializeField] float AttackRate=2f;
+    [SerializeField] int Weapon1Damage=40;
     [SerializeField] float AtkRange = 0.5f;
     [SerializeField]LayerMask Enemylayers;
+    LifeEnemy01 Enemy;
     void Start()
     {
         AtkPoint=GameObject.Find("AtkPoint").GetComponent<Transform>();
@@ -16,14 +20,19 @@ public class CombatPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space)){
-            Attack();
+        if(Time.time >=NextAttackTime){
+            Debug.Log("oi");
+            if(Input.GetKeyDown(KeyCode.Space)){
+                Attack();
+                NextAttackTime=Time.time+1f/AttackRate;
+            }
         }
+        
     }
     void Attack(){
         Collider2D[] HitEnemys=Physics2D.OverlapCircleAll(AtkPoint.position, AtkRange, Enemylayers);
-        foreach(Collider2D Enemy in HitEnemys){
-            Debug.Log("We hit "+Enemy);
+        foreach(Collider2D enemy in HitEnemys){
+            enemy.GetComponent<LifeEnemy01>().TakeDamage(Weapon1Damage);
         }
     }
     void OnDrawGizmosSelected() {
